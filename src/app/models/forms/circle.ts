f545705da247efcac710e4df2cd5b6a8f2e2bfd1
gummelhummel <fabian.els@outlook.com>
2017-08-17 22:21:30 +0200

    added render methods for circle and square

--- src/app/models/forms/circle.ts
+++ src/app/models/forms/circle.ts
@@ -1,12 +1,16 @@
 import { Point } from '../';
 export class Circle {
     public radius: number;
+    public point: Point;
 
-    constructor(radius: number) {
+    constructor(radius: number, point: Point) {
         this.radius = radius;
+        this.point = point;
     }
 
-    render(context: CanvasRenderingContext2D, point: Point) {
-        context.arc(point.x, point.y, this.radius, 0, 2 * Math.PI);
+    render(context: CanvasRenderingContext2D) {
+        context.beginPath();
+        context.arc(this.point.x, this.point.y, this.radius, 0, 2 * Math.PI);
+        context.stroke();
     }
 }
