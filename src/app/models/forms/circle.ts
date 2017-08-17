fbc5506e446393dde7f7265235d773ffa8127b66
gummelhummel <fabian.els@outlook.com>
2017-08-17 21:46:05 +0200

    added var radius and render func

--- src/app/models/forms/circle.ts
+++ src/app/models/forms/circle.ts
@@ -1,2 +1,12 @@
+import { Point } from '../';
 export class Circle {
+    public radius: number;
+
+    constructor(radius: number) {
+        this.radius = radius;
+    }
+
+    render(context: CanvasRenderingContext2D, point: Point) {
+        context.arc(point.x, point.y, this.radius, 0, 2 * Math.PI);
+    }
 }
